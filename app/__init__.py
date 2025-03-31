e671d15c0f7cc3304c39e389229947a6ab156edd
Arch-alain <alaind44i@gmail.com>
2025-03-31 02:36:56 +0200

    modified how to run the application

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,18 @@
-from flask import Flask
-from .utils.logging import setup_logging
-from .routes import init_routes
+# from flask import Flask
+# from .utils.logging import setup_logging
+# from .routes import init_routes
 
-def create_app():
-    app = Flask(__name__)
-    setup_logging()  # Configure logging
-    init_routes(app)  # Register routes
-    return app
+# def create_app():
+#     app = Flask(__name__)
+#     setup_logging()  # Configure logging
+#     init_routes(app)  # Register routes
+#     return app
 
 # app = create_app()
+
+from flask import Flask
+
+app = Flask(__name__)
+
+from app.routes import init_routes # Import routes after initializing Flask
+init_routes(app)
